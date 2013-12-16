c2783109231f965e0abd2e9431fcc508daf801d2
LE STUM Sébastien <lestums@gmail.com>
2013-12-16 12:47:28 +0100

    Exercise 2 Completion
    - Adding screenshot for report
    - Correction of errors

--- e2/serveur.c
+++ e2/serveur.c
@@ -5,72 +5,57 @@
 #include <sys/un.h>
 #include <netinet/in.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
 
-        int s_ecoute, res;
-
-        struct sockaddr_in myAdd;
-
-        s_ecoute = socket(AF_INET, SOCK_STREAM, 0);
-        if(s_ecoute == -1)
-        {
-                printf("Erreur de création de la socket!\n");
-                perror("L'erreur est la suivante ");
-                return 0;
-        }
-
-        myAdd.sin_family = AF_INET;
-        myAdd.sin_port = htons(atoi(argv[1]));
-        myAdd.sin_addr.s_addr = INADDR_ANY;
-
-        memset(myAdd.sin_zero, 0, 8);
-
-
-
-        res = bind(s_ecoute, (struct sockaddr*) &myAdd, sizeof(myAdd));
-
-        if(res == -1)
-        {
-                printf("Erreur lors du bind!\n");
-                return 0;
-        }
-
-        struct sockaddr_in client;
-
-        int taille = sizeof(client);
-        char requete[1000];
-				/* 1 client en attente de connexion */
-
-				if(listen(s_ecoute,1)==-1)
-				{
- 								printf("Erreur lors du listen!\n");
-                return 0;
-				}
+        struct sockaddr_in serverAdd;
+	struct sockaddr_in clientAdd;
 	
-				int s_service = accept(s_ecoute,(struct sockaddr*) &client, &taille);
+        int lSocket, lService, res;
+	int taille = sizeof(clientAdd);
+        char requete[2000];
+	char reponse[7] = "OK HTTP";
+	
+        lSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-				if(s_service==-1)
-				{
- 								printf("Erreur lors du accept!\n");
-                return 0;
-				}
+        if(lSocket == -1){
+                perror("Erreur lors de la création de la socket !\n");
+                return -1;
+        }
 
-				read(s_service, (void*) requete, sizeof(requete));
+        serverAdd.sin_family = AF_INET;
+        serverAdd.sin_port = htons(atoi(argv[1]));
+        serverAdd.sin_addr.s_addr = INADDR_ANY;
+        memset(serverAdd.sin_zero, 0, 8);
+        res = bind(lSocket, (struct sockaddr*) &serverAdd, sizeof(serverAdd));
 
-				write(s_service, (void*) requete, sizeof(requete));
-        /*int recev = recvfrom(d_s, requete, 1000, 0,(struct sockaddr*) &client, &taille);
-        if(recev == -1)
-        {
-                perror("ne reçoit rien");
-                return 0;
-        }*/
+        if(res == -1){
+                perror("Erreur lors du bind !\n");
+                return -1;
+        }
 
-        printf("La requete de firefox est  : \n%s", requete);
-				close(s_ecoute);		
-				close(s_service);
+        /* 1 clientAdd en écoute */
+	if(listen(lSocket,1) == -1){
+ 		perror("Erreur lors du listen !\n");
+                return -1;
+	}
+	
+	lService = accept(lSocket,(struct sockaddr*) &clientAdd, &taille);
 
-        printf("Tout marche!!!%d\n",getpid());
+	if(lService==-1){
+ 		perror("Erreur lors du accept !\n");
+                return -1;
+	}
+
+	
+	read(lService, (void*) requete, sizeof(requete));
+	fprintf(stdout,"La requete du navigateur est  : \n---------\n %s \n---------\n", requete);
+
+	write(lService, (void*) reponse, sizeof(reponse));
+	
+	close(lSocket);		
+	close(lService);
+
+        fprintf(stdout,"End of line...\n");
 				
         return 0;
 }
